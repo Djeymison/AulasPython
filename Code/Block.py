fae44e2c7f18ec0785c0c90aaadba0d63d28083f
Djeymison <Djeymisonwork@gmail.com>
2023-08-26 03:44:14 -0300

    A commit with a block code

---
+++ Code/Block.py
@@ -0,0 +1,22 @@
+def get(value): #start of method block
+
+    sold = (500)
+
+    if sold >= value: # start of block if
+
+        print("Get your chash")
+
+def  deposit(value): # A second method block
+    sold = 500
+    sold += value
+
+    print("you're deposited: {}, " "Your sold is: {}".format(value, sold))
+
+get(100) # call de funcion
+deposit(50) # call de funcion
+
+    # End of If block
+
+# End of method block
+    
+# Point: Both methods interact inside the block 
